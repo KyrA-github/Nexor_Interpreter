d15636c639d232345097947921c127ca850c25f3
KyrA-github <korenovsostav@gmail.com>
2024-04-05 01:02:16 +0300

    error

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -4,6 +4,46 @@ void Interpreter::InterpreterMainFunc(string main_source_file_directory, string 
     ReadingFileLineByLine(main_source_file_directory, '/'+ sourse_file_name);
 }
 
+string Interpreter::LineBetweenTokens(string token_1, string line) {
+    size_t start_pos = line.find(token_1);
+    if (start_pos == string::npos)
+        return ""; 
+
+    return token_1;
+}
+
+
+void Interpreter::ReadTokensFromLineFromFunction(string line)
+{
+    for (const auto& token : tokens) 
+    {
+        posed_token = line.find(token); 
+        //cout << "ffffff:"<< token << endl;
+        if (posed_token != string::npos)
+        {
+            cout << "GGGGGG:"<< token << endl;
+            cout << line << endl;
+            
+            temp_func = LineBetweenTokens(token, line);
+            if (temp_func == "int")
+            {
+                cout << "YYYYYY:"<< token << endl;
+                cout << line << endl;
+                cout << token << endl;
+                temp = LineBetweenTokens("int ", " = ", line);
+                cout << temp << endl;
+                setVariablesInteger(LineBetweenTokens("int", "=", line), 16);
+            }
+            else if (temp_func == "print")
+            {
+                temp = LineBetweenTokens("(", ")", line);
+                cout << temp << endl;
+                cout << getVariableInteger(temp) << endl;
+            }
+        }    
+    }
+}
+
 void Interpreter::ReadTokensFromLine()
 {    
     /*
@@ -36,15 +76,31 @@ void Interpreter::ReadTokensFromLine()
                         defineFunction(currentFunction, functionContent);
                         functionContent.clear();
                     }
-                    currentFunction = LineBetweenTokens(" ", "(");
-                    writing_content_func = true;
+                    currentFunction = LineBetweenTokens(" ", "(", line_file);
+                    if (line_file.back() == '{')
+                    {
+                        writing_content_func = true;
+                    }
+                }
+                else if (token == "$start")
+                {
+                    if (!currentFunction.empty())
+                    {
+                        defineFunction(currentFunction, functionContent);
+                        functionContent.clear();
+                    }
+                    currentFunction = "$start";
+                    if (line_file.back() == '{')
+                    {
+                        writing_content_func = true;
+                    }
                 }
             }    
         }
     }
     else
     {   
-        posed_token = line_file.find("}!"); 
+        posed_token = line_file.find("}"); 
         if (posed_token != string::npos)
         {
             defineFunction(currentFunction, functionContent);
@@ -55,6 +111,21 @@ void Interpreter::ReadTokensFromLine()
         else{
             functionContent += line_file + "\n";
         }
+    }
+
+}
+
+void Interpreter::ExecutionOfFunction(string function_content)
+{
+    istringstream iss(function_content);
+    string line;
+    while (getline(iss, line))
+    {
+        if (!line.empty())
+        {
+            ReadTokensFromLineFromFunction(line);
+        }
+        previous_line = line;
     }
 }
 
@@ -71,9 +142,10 @@ void Interpreter::ReadingFileLineByLine(string main_source_file_directory, strin
             {
                 lastCharline = line_file.back();
                 ReadTokensFromLine();
-            } 
+            }
+            previous_line = line_file;
         }
-        callFunction("main");
+        callFunction("$start");
     }
     else
     {
@@ -81,13 +153,13 @@ void Interpreter::ReadingFileLineByLine(string main_source_file_directory, strin
     }
 }
 
-string Interpreter::LineBetweenTokens(string token_1, string token_2)
+string Interpreter::LineBetweenTokens(string token_1, string token_2, string line)
 {
-    size_t start_pos = line_file.find(token_1);
-    size_t end_pos = line_file.find(token_2, start_pos);
+    size_t start_pos = line.find(token_1);
+    size_t end_pos = line.find(token_2, start_pos);
 
-    if (start_pos != std::string::npos && end_pos != std::string::npos) {
-        std::string result = line_file.substr(start_pos + 1, end_pos - (start_pos + 1));
+    if (start_pos != string::npos && end_pos != string::npos) {
+        string result = line.substr(start_pos + 1, end_pos - (start_pos + 1));
         return result;
     } else {
         return "";
@@ -97,16 +169,44 @@ string Interpreter::LineBetweenTokens(string token_1, string token_2)
 void Interpreter::callFunction(const string& name)
 {
     auto it = functionBlocks.find(name);
-    if (it != functionBlocks.end()) {
+    if (it != functionBlocks.end()) 
+    {
         // Вывод содержимого функции (для демонстрации)
-        std::cout << "Content of function " << name << ":\n" << it->second << std::endl;
+        cout << "Content of function " << name << ":\n" << it->second << endl;
         // Теперь здесь вы можете интерпретировать содержимое функции
-    } else {
-        std::cerr << "Error: Function " << name << " is not defined!" << std::endl;
+        ExecutionOfFunction(it->second);
+    } 
+    else
+    {
+        cerr << "Error: Function " << name << " is not defined!" << endl;
     }
 }
 
-void Interpreter::defineFunction(const std::string& name, const std::string& content)
+void Interpreter::defineFunction(const string& name, const string& content)
 {
     functionBlocks[name] = content;
+}
+
+void Interpreter::setVariablesInteger(const string& name, int value) {
+    variablesInteger[name] = value;
+}
+
+int Interpreter::getVariableInteger(const string& name) {
+    try
+    {
+        if (variablesInteger.find(name) != variablesInteger.end()) 
+        {
+            return variablesInteger[name];
+        } 
+        else 
+        {
+            cerr << "Error: Variable " << name << " is not defined!" << endl;
+        return 0; 
+        }
+    }
+    catch (const exception& error)
+    {
+        cerr << "Error: " << error.what() << endl;
+        return 0;
+    }
 }
