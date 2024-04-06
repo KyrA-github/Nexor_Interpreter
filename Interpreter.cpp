ba54b4ebec2da3179a1dc4e1d4e21d10a0240e15
KyrA-github <korenovsostav@gmail.com>
2024-04-06 22:55:24 +0300

    performing mathematical operations

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -1,49 +1,149 @@
 #include "Interpreter.hpp"
+
+//основная функция
 void Interpreter::InterpreterMainFunc(string main_source_file_directory, string sourse_file_name)
 {
     ReadingFileLineByLine(main_source_file_directory, '/'+ sourse_file_name);
 }
 
-string Interpreter::LineBetweenToken(string token_1, string line) {
-    size_t start_pos = line.find(token_1);
-    if (start_pos == string::npos)
-        return ""; 
-
-    return token_1;
-}
-
-
-void Interpreter::ReadTokensFromLineFromFunction(string line)
+// чтение файла
+void Interpreter::ReadingFileLineByLine(string main_source_file_directory, string sourse_file_name)
 {
-    for (const auto& token : tokens) 
+    ifstream main_source_file(main_source_file_directory + sourse_file_name);
+
+    if (main_source_file.is_open())
     {
-        posed_token = line.find(token); 
-        //cout << "ffffff:"<< token << endl;
-        if (posed_token != string::npos)
+        while (getline(main_source_file, line_file))
         {
-            cout << "GGGGGG:"<< token << endl;
-            cout << line << endl;
-            
-            temp_func = LineBetweenToken(token, line);
-            if (temp_func == "int")
+            if (!line_file.empty()) 
             {
-                cout << "YYYYYY:"<< token << endl;
-                cout << line << endl;
-                cout << token << endl;
-                temp = LineBetweenTokens("int ", " = ", line);
-                cout << temp << endl;
-                setVariablesInteger(LineBetweenTokens("int", "=", line), 16);
+                lastCharline = line_file.back();
+                ReadTokensFromLine();
             }
-            else if (temp_func == "print")
-            {
-                temp = LineBetweenTokens("(", ")", line);
-                cout << temp << endl;
-                cout << getVariableInteger(temp) << endl;
-            }
-        }    
+            previous_line = line_file;
+        }
+        callFunction("$start");
+    }
+    else
+    {
+        cerr << "Error: File not found" << endl;
     }
 }
 
+
+//
+string Interpreter::SearchVariablesByNameINT(unordered_map<string, int> value)
+{
+    for (const auto& pair : variablesInteger) {
+
+    }
+    return "";
+}
+
+void Interpreter::PerformingMathematicalOperations(string Variable1value, string Variable2value, string Variable3sum, char operation)
+{
+    bool value1 = false;
+    bool value2 = false;
+    int value1Int = 0;
+    int value2Int = 0;
+    int result = 0;
+    try
+    {
+        temp_int_list[0] = stoi(Variable1value);
+        value1 = true;
+    }
+    catch(...)
+    {
+    }
+    try
+    {
+        temp_int_list[1] = stoi(Variable2value);
+        value2 = true;
+    }
+    catch(...)
+    {
+    }
+    
+    if (value1) 
+    {
+        value1Int = temp_int_list[0]; 
+    }
+    else 
+    {
+        value1Int = getVariableInteger(Variable1value);
+    }
+
+    if (value2) 
+    {
+        value2Int = temp_int_list[1];
+    } 
+    else 
+    {
+        value2Int = getVariableInteger(Variable2value); 
+    }
+
+    switch (operation) {
+    case '+':
+        result = value1Int + value2Int;
+        break;
+    case '-':
+        result = value1Int - value2Int;
+        break;
+    case '*':
+        result = value1Int * value2Int;
+        break;
+    case '/':
+        result = value1Int / value2Int;
+        break;
+    default:
+        break;
+    }
+
+    setVariablesInteger(Variable3sum, result);
+
+}
+
+//выполнение токенов в функции
+void Interpreter::ReadTokensFromLineFromFunction(string line)
+{
+    if (LineContainsWord("int", line))
+    {
+        setVariablesInteger(LineBetweenTokens("int ", " =", line), stoi(LineBetweenTokens(" = ", ";", line)));
+    }
+    else if (LineContainsWord("print", line))
+    {
+        temp = LineBetweenTokens("(", ")", line);
+        cout << getVariableInteger(temp) << endl;
+    }
+    else if (LineContainsWord("+",line))
+    {
+        PerformingMathematicalOperations(LineBetweenTokens("= ", " + ", line), LineBetweenTokens(" + ", ";", line), LineBetweenTokens("    ", " =", line), '+');
+    }           
+    else if (LineContainsWord("-",line))
+    {
+        PerformingMathematicalOperations(LineBetweenTokens("= ", " - ", line), LineBetweenTokens(" - ", ";", line), LineBetweenTokens("    ", " =", line), '-');
+    }           
+    else if (LineContainsWord("*",line))
+    {
+        PerformingMathematicalOperations(LineBetweenTokens("= ", " * ", line), LineBetweenTokens(" * ", ";", line), LineBetweenTokens("    ", " =", line), '*');
+    }           
+    else if (LineContainsWord("/",line))
+    {
+        PerformingMathematicalOperations(LineBetweenTokens("= ", " / ", line), LineBetweenTokens(" / ", ";", line), LineBetweenTokens("    ", " =", line), '/');
+    }           
+
+    
+    // for (const auto& token : tokens) 
+    // {
+    //     posed_token = line.find(token); 
+    //     if (posed_token != string::npos)
+    //     {            
+            
+    //     }    
+    // }
+}
+
+//создание функций
 void Interpreter::ReadTokensFromLine()
 {    
     /*
@@ -115,6 +215,7 @@ void Interpreter::ReadTokensFromLine()
 
 }
 
+//чтение функции и выполнение
 void Interpreter::ExecutionOfFunction(string function_content)
 {
     std::istringstream iss(function_content);
@@ -131,50 +232,46 @@ void Interpreter::ExecutionOfFunction(string function_content)
 
 
 
-void Interpreter::ReadingFileLineByLine(string main_source_file_directory, string sourse_file_name)
-{
-    ifstream main_source_file(main_source_file_directory + sourse_file_name);
 
-    if (main_source_file.is_open())
-    {
-        while (getline(main_source_file, line_file))
-        {
-            if (!line_file.empty()) 
-            {
-                lastCharline = line_file.back();
-                ReadTokensFromLine();
-            }
-            previous_line = line_file;
-        }
-        callFunction("$start");
-    }
-    else
-    {
-        cerr << "Error: File not found" << endl;
-    }
-}
-
+// поиск строки межру символами
 string Interpreter::LineBetweenTokens(string token_1, string token_2, string line)
 {
-    size_t start_pos = line.find(token_1);
-    size_t end_pos = line.find(token_2, start_pos);
+    size_t startPos = line.find(token_1);
+    if (startPos == std::string::npos)
+        return ""; // Не найден начальный токен
 
-    if (start_pos != string::npos && end_pos != string::npos) {
-        string result = line.substr(start_pos + 1, end_pos - (start_pos + 1));
-        return result;
-    } else {
-        return "";
-    }
+    startPos += token_1.length();
+    size_t endPos = line.find(token_2, startPos);
+    if (endPos == std::string::npos)
+        return ""; // Не найден конечный токен
+
+    return line.substr(startPos, endPos - startPos);
 }
 
+//поиск симавола/слова в строке
+bool Interpreter::LineContainsWord(const std::string& word, const std::string& line) 
+{
+        size_t pos = line.find(word);
+
+        while (pos != std::string::npos) 
+        {
+            if ((pos == 0 || !isalnum(line[pos - 1])) && (pos + word.length() == line.length() || !isalnum(line[pos + word.length()]))) 
+            { 
+                return true;
+            }
+            pos = line.find(word, pos + 1); 
+        }
+
+        return false;
+}
+
+//чтение функции
 void Interpreter::callFunction(const string& name)
 {
     auto it = functionBlocks.find(name);
     if (it != functionBlocks.end()) 
     {
-        // Вывод содержимого функции (для демонстрации)
-        cout << "Content of function " << name << ":\n" << it->second << endl;
-        // Теперь здесь вы можете интерпретировать содержимое функции
+        //cout << "Content of function " << name << ":\n" << it->second << endl << "||" << endl;
         ExecutionOfFunction(it->second);
     } 
     else
@@ -183,15 +280,18 @@ void Interpreter::callFunction(const string& name)
     }
 }
 
+// создание функции
 void Interpreter::defineFunction(const string& name, const string& content)
 {
     functionBlocks[name] = content;
 }
 
+//задать переменную
 void Interpreter::setVariablesInteger(const string& name, int value) {
     variablesInteger[name] = value;
 }
 
+//воспользоваться переменной
 int Interpreter::getVariableInteger(const string& name) {
     try
     {
--- Interpreter.hpp
+++ Interpreter.hpp
@@ -14,23 +14,25 @@ public:
 
     void InterpreterMainFunc(string main_source_file_directory, string sourse_file_name);
 private:
-    void ReadTokensFromLine();
     void ReadingFileLineByLine(string main_source_file_directory, string sourse_file_name);
-
     void ReadTokensFromLineFromFunction(string line);
 
+    void ReadTokensFromLine();
     void ExecutionOfFunction(string function_content);
 
     int getVariableInteger(const string& name);
     void setVariablesInteger(const string& name, int value);
 
-
     void defineFunction(const std::string& name, const std::string& content);
     void callFunction(const string& name);
 
     string LineBetweenTokens(string token_1, string token_2, string line);
-    string LineBetweenToken(string token_1, string line);
+    bool LineContainsWord(const std::string& word, const std::string& line);
 
+    string SearchVariablesByNameINT(unordered_map<string, int> value);
+
+    //operators + - * /
+    void PerformingMathematicalOperations(string Variable1value, string Variable2value, string Variable3sum, char operation);
 
     string line_file;
     string previous_line;
@@ -44,10 +46,15 @@ private:
     string right_part_line;
 
     string temp;
+    string temp_list[10];
+    int temp_int;
+    int temp_int_list[10];
+    float temp_float;
     string temp_func;
 
     string currentFunction;
     string functionContent;
+    string error;
 
     string tokens[10] =
     {
